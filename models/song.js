d78d101c9329e099bf16f93f0b692c248055c4f8
Dan Florio <dflo@danflorio.com>
2017-08-23 16:04:03 -0400

    add angular2 front end

--- models/song.js
+++ models/song.js
@@ -34,34 +34,3 @@ module.exports.createSong = function(newSong, callback) {
 module.exports.deleteSongById = function(id, callback) {
     Song.findByIdAndRemove(id, callback);
 }
-
-
-/*
-D                            G          A       D       G    A
-Early in the morning too hungover to go back to sleep
-D                         G              A    D       G    A
-Every sound is amplified, every light so dizzying
-G                         A
-Listen for a while to the neighbors having sex
-G                             A
-Wishing I could lay my aching head upon your breast
-        G      G/F#   A
-Can't I dream another dream?
-        G                                A
-Can't I close my eyes and wander back to sleep?
-
-        D            G    Bm
-But I'm daydreaming about you
-A                   G
-   I know that it's wrong
-         D            G    Bm
-That I'm daydreaming about you
-    A                                  G  G/F#  A  D
-And I've been daydreaming for so long
-*/
-
-/*
-"D|||||    G   |||A||D|   G|   A"
-"Early|in|the|morning|too|hungover|to|go|back|to|sleep||"
-"D||||G|||A    D|   G|   A"
-*/
